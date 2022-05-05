7ef870168ac8308680c52f13cfad16e511e45b50
huangmi <huangmi@newrank.cn>
2022-05-05 18:21:46 +0800

    first commit

---
+++ src/popup/index.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react"
+import ReactDOM from "react-dom"
+import { Button } from "antd"
+// import 'virtual:windi.css';
+// import "antd/es/button/style/index.css"
+
+const Popup = () => {
+  const [count, setCount] = useState(0)
+  const [currentURL, setCurrentURL] = useState<string>()
+
+  useEffect(() => {
+    chrome.action.setBadgeText({ text: count.toString() })
+  }, [count])
+
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      setCurrentURL(tabs[0].url)
+    })
+  }, [])
+
+  const changeBackground = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const tab = tabs[0]
+      if (tab.id) {
+        chrome.tabs.sendMessage(
+          tab.id,
+          {
+            color: "#555555",
+          },
+          (msg) => {
+            console.log("result message:", msg)
+          }
+        )
+      }
+    })
+  }
+
+  return (
+    <div style={{ width: "360px" }}>
+      <Button>xxx</Button>
+      <h1>popup</h1>
+      <div className="bg-dark-100">hhh</div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Popup />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
